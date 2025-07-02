31629415e2af117f60476e326589801853349077
PatrickMWAMBA <164631797+PatrickMWAMBA@users.noreply.github.com>
2025-07-02 13:03:16 +0200

    Remove duplicate JPA annotations

--- TaskManagerApp/src/main/java/com/taskmanager/app/TaskManagerAppApplication.java
+++ TaskManagerApp/src/main/java/com/taskmanager/app/TaskManagerAppApplication.java
@@ -2,14 +2,8 @@ package com.taskmanager.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "com.taskmanager.app.repository")
-@EntityScan(basePackages = "com.taskmanager.app.entity")
 public class TaskManagerAppApplication {
     public static void main(String[] args) {
         SpringApplication.run(TaskManagerAppApplication.class, args);
